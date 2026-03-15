a29798c6506df6a7c82e0d73a3e6d6ed601ca173
chitrank2050 <chitrank2050@gmail.com>
2026-03-15 20:39:38 +0530

    feat: Add `api_lifespan` module and expose it in the `core` package.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,4 +1,5 @@
+from .api_lifespan import api_lifespan
 from .bootstrap import bootstrap
 from .logger import logger, setup_logger
 
-__all__ = ["bootstrap", "logger", "setup_logger"]
+__all__ = ["api_lifespan", "bootstrap", "logger", "setup_logger"]
